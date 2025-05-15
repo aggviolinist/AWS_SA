422db77693ee317944d0ed4b004383b730b12ad8
sCuLpToR 699 <mulandimaei76@gmail.com>
2025-05-15 04:51:25 +0000

    Modularity to gitpod file

---
+++ s3/etags/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0-beta1"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "my-favourite-bucko" {
+}
+
+resource "aws_s3_object" "object-in-my-bucko" {
+  bucket = my.best-bucko
+  key    = "myfile.txt"
+  source = "myfile.txt"
+  }
